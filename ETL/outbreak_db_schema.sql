c8ea069840305e881d00b540eafbde354e4b7791
Olufemi-Olarewaju <olarewajufemi99@gmail.com>
2023-12-04 21:07:16 -0500

    Database Schema

---
+++ ETL/outbreak_db_schema.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE "Outbreak_Table" (
+    "ID" INTEGER   NOT NULL,
+    "Institution_Name" VARCHAR   NOT NULL,
+    "Institution_Address" VARCHAR   NOT NULL,
+    "Outbreak_Setting" VARCHAR   NOT NULL,
+    "Type_of_Outbreak" VARCHAR   NOT NULL,
+    "Causative_Agent" VARCHAR   NOT NULL,
+    "Date_Outbreak_Began" DATE   NOT NULL,
+    "Date_Declared_OveR" DATE   NOT NULL,
+    "Outbreak_Status" VARCHAR   NOT NULL,
+    "Longitude" DEC   NOT NULL,
+    "Latitude" DEC   NOT NULL,
+    CONSTRAINT "pk_Outbreak_Table" PRIMARY KEY (
+        "ID"
+    )
+);
+
