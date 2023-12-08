987bac3995e4c7220a4332fe457087089c37a907
Olufemi Emmanuel Olarewaju <142542433+Olufemi-Olarewaju@users.noreply.github.com>
2023-12-07 19:05:11 -0500

    Update outbreak_db_schema.sql

--- ETL/outbreak_db_schema.sql
+++ ETL/outbreak_db_schema.sql
@@ -6,7 +6,7 @@
     "Type_of_Outbreak" VARCHAR   NOT NULL,
     "Causative_Agent" VARCHAR   NOT NULL,
     "Date_Outbreak_Began" DATE   NOT NULL,
-    "Date_Declared_Over" DATE   NOT NULL,
+    "Date_Declared_Over" DATE,
     "Outbreak_Status" VARCHAR   NOT NULL,
     "Longitude" DEC   NOT NULL,
     "Latitude" DEC   NOT NULL,
