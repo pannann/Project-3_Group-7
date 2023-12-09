dedc29c9610c29a99087a8ef21132c88c6174960
fannysigouin <fannysigouin@Fannys-MacBook-Pro.local>
2023-12-09 16:42:46 -0500

    basic map working, fixing markers

--- static/js/outbreaks_map.js
+++ static/js/outbreaks_map.js
@@ -7,35 +7,73 @@ const outbreaks_url = "http://127.0.0.1:5000/api/outbreaks";
 //     console.log(data);
 // });
 
-// code from https://developers.google.com/maps/documentation/javascript/overview#javascript
-// let map;
+// function to create the map
+function createMap(outbreaks) {
+    // create background tile layer - OMS tiles
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-// async function initMap() {
-//   const { Map } = await google.maps.importLibrary("maps");
+    // create baseMaps object to hold street tiles layer
+    let baseMaps = {
+        Street: street 
+    };
 
-//   map = new Map(document.getElementById("map_div"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 8,
-//   });
-// }
+    // create overlayMaps object to hold outbreaks layer
+    let overlayMaps = {
+        'Outbreaks': outbreaks
+    };
 
-// initMap();
+    // create map with the center as the lat/lon of Toronto
+    let myMap = L.map('map', {
+        center: [43.6532, -79.3832],
+        zoom: 7,
+        layers: [street, outbreaks]
+    });
 
-// function to draw map
-// function drawMap() {
-//     d3.json(outbreaks_url).then(function(response) {
-//         // store response in a variable
-//         let data = response;
-//         // store coordinates in an array of arrays
-//         let points = [];
-//         data.forEach((value) => {
-//             points.push([value[10], value[9]])
-//         });
-//         let trace2 = google.visualization.arrayToDataTable(points);
-//         let map = new google.visualization.Map(document.getElementById('map_div'));
-//         map.draw(trace2, {
-//             showTooltip: true,
-//             showInfoWindow: true
-//         });
-//     })
-// }
+    // create layer control and add to map
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+}
+
+// function to create the markers
+function createMarkers(response) {
+    // empty array to hold markers
+    let outbreakMarkers = [];
+
+    // loop through response
+    for (let i = 0; i < response.length; i++) {
+        // for each outbreak, create a marker
+        let outbreak = response[i];
+        // bind a popup with the outbreak's information
+        outbreakMarkers.push(L.marker([outbreak[10], outbreak[9]]).bindPopup(`<h3>${outbreak[1]}</h3><br><h4>Location: ${outbreak[2]}, ${outbreak[3]}</h4><br><h4>Type of Outbreak: ${outbreak[4]}, ${outbreak[5]}</h4><br><h4>Start: ${outbreak[6]} End: ${outbreak[7]}</h4>`));
+    }
+
+    // pass layer group from outbreakMarkers to createMap function
+    createMap(L.layerGroup(outbreakMarkers));
+}
+
+// function to set marker size based on outbreak duration
+function markerSize(start, end) {
+    let start_date = new Date(start);
+    let end_date = new Date(end);
+    let difference = Math.abs(end_date - start_date);
+    let duration = difference/(1000 * 3600 * 24);
+    return duration * 5;
+}
+
+// function to set marker colour based on type of outbreak
+function markerColour(type) {
+    if (type == 'Respiratory') {
+        return "#219ebc";
+    } else if (type == 'Enteric') {
+        return '#9d4edd';
+    } else if (type == 'Other') {
+        return '#ffb703';
+    }
+}
+
+
+// switch to asynchronous mode with promise function and call createMarkers function
+d3.json(outbreaks_url).then(function(response) {
+    createMarkers(response);
+})
